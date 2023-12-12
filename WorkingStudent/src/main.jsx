899a9af6eed4336adeb1d78d624329ee501d071e
FilipKCX <email@example.comgit config --global user.emaial>
2023-12-12 17:29:19 +0100

    Navbar top

--- WorkingStudent/src/main.jsx
+++ WorkingStudent/src/main.jsx
@@ -9,7 +9,7 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <LogInButtonNoLogIn/>
-    <Scrollbar/>
     <Navbar/>
+    <Scrollbar/>
   </React.StrictMode>,
 )
