b21f4dedb7f645aba6fa9a1e0e9581c3652809d7
FilipKCX <email@example.comgit config --global user.emaial>
2023-12-20 23:01:02 +0100

    Navbar

--- src/components/BottomNavbar.jsx
+++ src/components/BottomNavbar.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './BottomNavbar.css';
 import { Link } from "react-router-dom"
-import wostuLogo from '../assets/WS_logo'
 
 
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,7 +25,7 @@ import wostuLogo from '../assets/WS_logo.png'
             </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
+           <Nav className="ms-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#chat">Chat</Nav.Link>
             <Nav.Link href="#profil">Profil</Nav.Link>
