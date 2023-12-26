355990f1f0d7f8316ec2013c06b14045e015b68d
FilipKCX <email@example.comgit config --global user.emaial>
2023-12-26 23:09:04 +0100

    aaa

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -8,7 +8,7 @@ import {Link } from "react-router-dom";
 const Header = () => {
     return (
         <Link to="/HomeUser">
-            <div className='logo-link'>
+            <div className='logo-llink'>
                 WorkingStudent
             </div>    
         </Link>
--- src/components/TopNavbarUser.jsx
+++ src/components/TopNavbarUser.jsx
@@ -4,8 +4,8 @@ import {Link} from "react-router-dom"
 
 const TopNavbarUser = () => {
   return (
-    <div className="top-navbar">
-      <div className="topnav-links">
+    <div className="top-nnavbar">
+      <div className="topnnav-links">
        <Link to="/HomeUser">Home</Link>
        <Link to="/profilepage">Profil</Link>
        <Link to="/likes">Likes</Link>
