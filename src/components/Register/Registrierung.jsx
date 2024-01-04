e1627254099d05b442b2fa667ce0ece6d0dea270
wlgithub1k <wladislaw.tarassov@studium.uni-hamburg.de>
2024-01-04 19:43:07 +0100

    Logo

--- src/components/Register/Registrierung.jsx
+++ src/components/Register/Registrierung.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import React, { useState } from 'react';
 import './Registrierung.css';
 import { Link } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
