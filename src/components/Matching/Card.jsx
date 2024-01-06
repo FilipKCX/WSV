d71fecc54ff300443e705c443e9a66adf54e79ac
muhammeduhh <online-ma@outlook.de>
2024-01-06 15:36:10 +0100

    y

--- src/components/Matching/Card.jsx
+++ src/components/Matching/Card.jsx
@@ -8,40 +8,42 @@ function KitchenSinkExample() {
         <div className="mt-4 text-center">
             <div className="container">
                 <div className="row justify-content-center">
-                    <div className="col-md-4" style={{ marginBottom: '20px' }}> 
+                    <div className="col-md-4" style={{ marginBottom: '20px' }}>
+
                         <Card style={{ width: '18rem' }}> {/* erste Karte */}
                             <Card.Img variant="top" src="holder.js/100px180?text=Image" cap className='card-img-top' />
                             <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
+                                <Card.Title>Name</Card.Title>
                                 <Card.Text>
-                                    Some quick example text to build on the card title and make up the
-                                    bulk of the card's content.
+                                    Beschreibungen
                                 </Card.Text>
                             </Card.Body>
                             <ListGroup className="list-group-flush">
-                                <ListGroup.Item>Cras justo odio</ListGroup.Item>
-                                <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-                                <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+                            <ListGroup.Item>Studiengang:</ListGroup.Item>
+                                <ListGroup.Item>Semesteranzahl:</ListGroup.Item>
+                                <ListGroup.Item>Berufserfahrung:</ListGroup.Item>
+                                <ListGroup.Item>zeitliche Verfügbarkeit:</ListGroup.Item>
                             </ListGroup>
                             <Card.Body>
                                 <Button variant="primary" className='chat-button'>Chat starten!</Button>
                             </Card.Body>
                         </Card>
                     </div>
+
                     <div className="col-md-4">
                         <Card style={{ width: '18rem' }}> {/* zweite Karte */}
                             <Card.Img variant="top" src="holder.js/100px180?text=Image" cap className='card-img-top' />
                             <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
+                                <Card.Title>Name</Card.Title>
                                 <Card.Text>
-                                    Some quick example text to build on the card title and make up the
-                                    bulk of the card's content.
+                                    Beschreibungen
                                 </Card.Text>
                             </Card.Body>
                             <ListGroup className="list-group-flush">
-                                <ListGroup.Item>Cras justo odio</ListGroup.Item>
-                                <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-                                <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+                            <ListGroup.Item>Studiengang:</ListGroup.Item>
+                                <ListGroup.Item>Semesteranzahl:</ListGroup.Item>
+                                <ListGroup.Item>Berufserfahrung:</ListGroup.Item>
+                                <ListGroup.Item>zeitliche Verfügbarkeit:</ListGroup.Item>
                             </ListGroup>
                             <Card.Body>
                                 <Button variant="primary" className='chat-button'>Chat starten!</Button>
@@ -49,19 +51,20 @@ function KitchenSinkExample() {
                         </Card>
                     </div>
                     <div className="col-md-4">
+
                         <Card style={{ width: '18rem' }}> {/* dritte Karte */}
                             <Card.Img variant="top" src="holder.js/100px180?text=Image" cap className='card-img-top' />
                             <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
+                                <Card.Title>Name</Card.Title>
                                 <Card.Text>
-                                    Some quick example text to build on the card title and make up the
-                                    bulk of the card's content.
+                                    Beschreibungen
                                 </Card.Text>
                             </Card.Body>
                             <ListGroup className="list-group-flush">
-                                <ListGroup.Item>Cras justo odio</ListGroup.Item>
-                                <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-                                <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+                                <ListGroup.Item>Studiengang:</ListGroup.Item>
+                                <ListGroup.Item>Semesteranzahl:</ListGroup.Item>
+                                <ListGroup.Item>Berufserfahrung:</ListGroup.Item>
+                                <ListGroup.Item>zeitliche Verfügbarkeit:</ListGroup.Item>
                             </ListGroup>
                             <Card.Body>
                                 <Button variant="primary" className='chat-button'>Chat starten!</Button>
--- src/components/Matching/Filter.jsx
+++ src/components/Matching/Filter.jsx
@@ -1,28 +1,143 @@
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import './Filter.css';
 
-function BasicExample() {
+function DropdownForm() {
+    const [showForm, setShowForm] = useState(false);
+    const [studiengang, setStudiengang] = useState('');
+    const [semester, setSemester] = useState([]);
+    const [berufserfahrung, setBerufserfahrung] = useState('');
+    const [arbeitszeit, setArbeitszeit] = useState('');
+
+    const handleButtonClick = () => {
+        setShowForm(!showForm);
+    };
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+
+        // Hier kannst du die ausgewählten Werte verwenden
+        console.log('Studiengang:', studiengang);
+        console.log('Semester:', semester);
+        console.log('Berufserfahrung:', berufserfahrung);
+        console.log('Arbeitszeit:', arbeitszeit);
+
+        // Formular zurücksetzen
+        setStudiengang('');
+        setSemester([]);
+        setBerufserfahrung('');
+        setArbeitszeit('');
+
+        // Dropdown-Formular ausklappen
+        setShowForm(false);
+    };
+
     return (
-        <Row xs={1} md={2} lg={3} className="g-4">
-            {Array.from({ length: 6 }).map((_, idx) => (
-                <Col key={idx}>
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the
-                                bulk of the card's content.
-                            </Card.Text>
-                            <Button variant="primary">Go somewhere</Button>
-                        </Card.Body>
-                    </Card>
-                </Col>
-            ))}
-        </Row>
+        <div className="dropdown-form">
+            <Button onClick={handleButtonClick} variant="primary" id="filter-button">
+                Filter
+            </Button>
+
+            {showForm && (
+                <Form onSubmit={handleFormSubmit} className="horizontal-form">
+                    <div className="horizontal-container">
+                        <Form.Group className="horizontal-options horizontal-inputbox">
+                            <Form.Label>Studiengang:</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Studiengang eingeben"
+                                value={studiengang}
+                                onChange={(e) => setStudiengang(e.target.value)}
+                            />
+                        </Form.Group>
+
+                        <Form.Group controlId="formSemester">
+                            <Form.Label>Semesteranzahl:</Form.Label>
+                            <div className="horizontal-options">
+                                <Form.Check
+                                    type="checkbox"
+                                    label="1. Semester"
+                                    checked={semester.includes('1. Semester')}
+                                    onChange={() => setSemester(['1. Semester'])}
+                                />
+                                <Form.Check
+                                    type="checkbox"
+                                    label="2. Semester"
+                                    checked={semester.includes('2. Semester')}
+                                    onChange={() => setSemester(['2. Semester'])}
+                                />
+                                <Form.Check
+                                    type="checkbox"
+                                    label="3. Semester"
+                                    checked={semester.includes('3. Semester')}
+                                    onChange={() => setSemester(['3. Semester'])}
+                                />
+                            </div>
+                        </Form.Group>
+
+                        <Form.Group controlId="formBerufserfahrung">
+                            <Form.Label>Berufserfahrung:</Form.Label>
+                            <div className="horizontal-options">
+                                <Form.Check
+                                    type="checkbox"
+                                    label="0 Jahre"
+                                    checked={berufserfahrung === '0 Jahre'}
+                                    onChange={() => setBerufserfahrung('0 Jahre')}
+                                />
+                                <Form.Check
+                                    type="checkbox"
+                                    label="1 Jahr"
+                                    checked={berufserfahrung === '1 Jahr'}
+                                    onChange={() => setBerufserfahrung('1 Jahr')}
+                                />
+                                <Form.Check
+                                    type="checkbox"
+                                    label="2 Jahre"
+                                    checked={berufserfahrung === '2 Jahre'}
+                                    onChange={() => setBerufserfahrung('2 Jahre')}
+                                />
+                                <Form.Check
+                                    type="checkbox"
+                                    label="3+ Jahre"
+                                    checked={berufserfahrung === '3+ Jahre'}
+                                    onChange={() => setBerufserfahrung('3+ Jahre')}
+                                />
+                            </div>
+                        </Form.Group>
+
+                        <Form.Group controlId="formArbeitszeit">
+                            <Form.Label>Arbeitszeit:</Form.Label>
+                            <div className="horizontal-options">
+                                <Form.Check
+                                    type="checkbox"
+                                    label="8 Stunden pro Woche"
+                                    checked={arbeitszeit === '8 Stunden pro Woche'}
+                                    onChange={() => setArbeitszeit('8 Stunden pro Woche')}
+                                />
+                                <Form.Check
+                                    type="checkbox"
+                                    label="10 Stunden pro Woche"
+                                    checked={arbeitszeit === '10 Stunden pro Woche'}
+                                    onChange={() => setArbeitszeit('10 Stunden pro Woche')}
+                                />
+                                <Form.Check
+                                    type="checkbox"
+                                    label="15 Stunden pro Woche"
+                                    checked={arbeitszeit === '15 Stunden pro Woche'}
+                                    onChange={() => setArbeitszeit('15 Stunden pro Woche')}
+                                />
+                            </div>
+                        </Form.Group>
+
+                    </div>
+                    <Button variant="primary" type="submit" id="apply-button">
+                        Anwenden
+                    </Button>
+                </Form>
+            )}
+        </div>
     );
 }
 
-export default BasicExample;
+export default DropdownForm;
