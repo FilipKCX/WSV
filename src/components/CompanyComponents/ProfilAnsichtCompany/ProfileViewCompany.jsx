8a50c404c8d4dbab8446bbcb38d0eeffb3e8ff6a
FilipKCX <email@example.comgit config --global user.emaial>
2024-01-18 01:56:06 +0100

    fgh

--- src/components/CompanyComponents/ProfilAnsichtCompany/ProfileViewCompany.jsx
+++ src/components/CompanyComponents/ProfilAnsichtCompany/ProfileViewCompany.jsx
@@ -3,16 +3,23 @@ import './ProfileViewCompany.css';
 import { Formik, Field, ErrorMessage } from 'formik';
 import * as yup from 'yup';
 import React, { useState } from 'react';
+import { getHTTPRequest } from '../../serverPackage';
 
 
-const validationSchema = yup.object({
-    unternehmensName: yup.string().matches(/^[a-zA-Z\s]+$/, 'Nur Buchstaben erlaubt'),
-    email: yup.string().email('Ungültige E-Mail-Adresse'),
-    standort: yup.string().matches(/^[a-zA-Z\s]+$/, 'Nur Buchstaben erlaubt'),
-    beschreibung: yup.string().required('Beschreibung ist erforderlich'),
-});
+
 
 const Unternehmensprofil = () => {
+    const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [standort, setStandort] = useState('');
+  const [wersw, setWersw] = useState('');
+  const [waswb, setWaswb] = useState('');
+  const [karriere, setKarriere] = useState('');
+  const [geschichte, setGeschichte] = useState('');
+  const [faehigkeiten, setFaehigkeiten] = useState('');
+  const [logo, setLogo] = useState('none.jpg')
+  const [selectedImagePath, setSelectedImagePath] = useState('');
+  
     const saveCompanyProfile = (values) => {
         console.log('Speichern der Unternehmensdaten...');
         console.log('Profilbild:', profilbild);
@@ -28,65 +35,76 @@ const Unternehmensprofil = () => {
 
   const [profilBild, setProfilBild] = useState(null);
 
-  const handleBildAuswahl = (event) => {
-      if (event.target.files && event.target.files[0]) {
-          setProfilBild(URL.createObjectURL(event.target.files[0]));
-      }
+  const handleImageSelection = (event) => {
+    const file = event.target.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+      const fileName = file.name;
+      reader.onload = () => {
+        setSelectedImagePath(reader.result);
+        const imageData = reader.result;
+        const base64Image = `data:image/jpeg;base64,${imageData}`;
+        setProfilBild(base64Image);
+        setLogo(fileName)
+      };
+
+      reader.readAsDataURL(file);
+    }
   };
 
   const triggerFileInput = () => {
       document.getElementById('profilbild-input').click();
   };
+  let usID = sessionStorage.getItem('userID')
+  let paramArray = [usID, name, email, standort, wersw, waswb, karriere, geschichte , logo];
+  console.log(paramArray)
 
+  async function handleProfileCreation() {
+    const apiResponse = await getHTTPRequest("createCompanyProfile", paramArray);
+    return;
+  }
     return (
-        <Formik
-            initialValues={{
-                unternehmensName: '',
-                email: '',
-                standort: '',
-                beschreibung: '',
-                angebote: '',
-                karriere: '',
-                geschichte: '',
-            }}
-            validationSchema={validationSchema}
-            onSubmit={(values) => saveCompanyProfile(values)}
-        >
-            <Form>
-                            <Container className="profil-container">
-                      <Row className="justify-content-md-center profil-row">
+        
+                <Container className="profil-container">
+                    <Row className="justify-content-md-center profil-row">
                         <Col md={6} className="profil-col">
                           <div className="profil-bild-container" onClick={triggerFileInput}>
-                            <Image src={profilBild || "platzhalter-bild-url.jpg"} roundedCircle className="profil-bild" />
+                            <Image src={selectedImagePath || "platzhalter-bild-url.jpg"} 
+                            roundedCircle
+                             className="profil-bild" />
+
                             <div>Klicken, um Foto hinzuzufügen</div>
-                            <input type="file" id="profilbild-input" hidden onChange={handleBildAuswahl} />
+                            <input type="file" id="profilbild-input" hidden onChange={handleImageSelection} />
                           </div>
                             <Form.Group>
-                                <Field
+                                <Form.Control
                                     type="text"
                                     placeholder="Unternehmensname"
                                     name="unternehmensName"
                                     className="profil-input"
+                                    onChange={e => setName(e.target.value)}
                                 />
-                                <ErrorMessage name="unternehmensName" component="div" className="error-message" />
+                                
                             </Form.Group>
                             <Form.Group>
-                                <Field
+                                <Form.Control
                                     type="text"
                                     placeholder="E-Mail"
                                     name="email"
                                     className="profil-input"
-                                />
-                                <ErrorMessage name="email" component="div" className="error-message" />
+                                    onChange={e => setEmail(e.target.value)}
+                                />                              
                             </Form.Group>
                             <Form.Group>
-                                <Field
+                                <Form.Control
                                     type="text"
                                     placeholder="Standort"
                                     name="standort"
                                     className="profil-input"
+                                    onChange={e => setStandort(e.target.value)}
                                 />
-                                <ErrorMessage name="standort" component="div" className="error-message" />
+                                
                             </Form.Group>
                         </Col>
                         <Col md={6}>
@@ -99,8 +117,9 @@ const Unternehmensprofil = () => {
                                         rows={3}
                                         name="beschreibung"
                                         className="profil-input"
+                                        onChange={e => setWersw(e.target.value)}
                                     />
-                                    <ErrorMessage name="beschreibung" component="div" className="error-message" />
+                                   
                                 </Card.Body>
                             </Card>
                             <Card className="profil-card">
@@ -112,6 +131,7 @@ const Unternehmensprofil = () => {
                                         rows={3}
                                         name="angebote"
                                         className="profil-input"
+                                        onChange={e => setWaswb(e.target.value)}
                                     />
                                 </Card.Body>
                             </Card>
@@ -124,6 +144,7 @@ const Unternehmensprofil = () => {
                                         rows={3}
                                         name="karriere"
                                         className="profil-input"
+                                        onChange={e => setKarriere(e.target.value)}
                                     />
                                 </Card.Body>
                             </Card>
@@ -136,19 +157,18 @@ const Unternehmensprofil = () => {
                                         rows={3}
                                         name="geschichte"
                                         className="profil-input"
+                                        onChange={e => setGeschichte(e.target.value)}
                                     />
                                 </Card.Body>
                             </Card>
                             <div className="d-flex justify-content-end mb-3">
-                                <Button type="submit" variant="primary" className="speichern-button">
+                                <Button type="submit" variant="primary" className="speichern-button" onClick={handleProfileCreation}>
                                     Speichern
                                 </Button>
                             </div>
                         </Col>
                     </Row>
                 </Container>
-            </Form>
-        </Formik>
     );
 };
 
--- src/components/UserComponents/Likes/LikeMenu.jsx
+++ src/components/UserComponents/Likes/LikeMenu.jsx
@@ -1,35 +1,82 @@
 import React from 'react';
-import './LikeMenu.css'; 
+import './LikeMenu.css';
+import { getHTTPRequest } from '../../serverPackage';
+import { useState , useEffect} from 'react';
 
-const LikeMenu = ({ selectLike, selectedLike }) => {
+export default function LikeMenu  ({ selectLike, selectedLike,  })  
+{
+  const [isLoading, setIsLoading] = useState(true);
+  const [likeUsers, setLikeUsers] = useState([12, 16 , 18]);
+  const uID = sessionStorage.getItem('userID');
+  const param = [uID];
+
   const handleLikeSelect = (likeId) => {
     selectLike(likeId);
   };
+  
+  
+  useEffect(() => {
+    const fetchUsers = async () => {
+           const apiResponse = await getHTTPRequest("getLikes", param);
+           const newArray = JSON.parse(apiResponse)
+           console.log(newArray)
+           setIsLoading(false)
+           //setLikeUsers(newArray);        
+         }
+    fetchUsers();
+    
+       },[]); 
 
-  return (
+
+       if (isLoading) {
+        return <div>Loading...</div>;
+      }
+
+  return <>
+
     <div className="like-menu">
       <div className="like-options">
-        <div
-          className={`like-option ${selectedLike === 'like1' ? 'active' : ''}`}
-          onClick={() => handleLikeSelect('like1')}
-        >
-          <div className="like-box">
-            <h3>Like 1</h3>
-            <p>Description or preview here...</p>
+        {likeUsers.map((company) => (
+          <div
+            key={company}
+            className={`like-option ${selectedLike === company ? 'active' : ''}`}
+            onClick={() => handleLikeSelect(company)}
+          >
+            <LikeBox company={company} />
           </div>
-        </div>
-        <div
-          className={`like-option ${selectedLike === 'like2' ? 'active' : ''}`}
-          onClick={() => handleLikeSelect('like2')}
-        >
-          <div className="like-box">
-            <h3>Like 2</h3>
-            <p>Description or preview here...</p>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
-  );
+    </>
 };
 
-export default LikeMenu;
+const LikeBox = ({ company }) => {
+
+  useEffect(() => {
+    const fetchData = async () => {
+      console.log(company);
+      const param = [company];
+      const apiResponse = await getHTTPRequest("getCompanyInfox", param);
+      const sortArray = JSON.parse(apiResponse);
+      const selectedArray = sortArray[0];
+
+      // Extract student data into a single object
+      const companyData = {
+        name: selectedArray[1],
+        study: selectedArray[4],
+        graduation: selectedArray[5],
+        workingHours: selectedArray[9],
+        experience: selectedArray[9],
+      };
+    }
+
+  return (
+    <div className="like-box">
+      <h3></h3>
+      <p>Description or preview here...</p>
+    </div>
+  );
+    }, []);
+};
+
+
