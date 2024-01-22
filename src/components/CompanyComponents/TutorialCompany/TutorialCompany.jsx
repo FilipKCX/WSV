4718ff6cff89c0c876efb8e525e72da942ae3ddd
muhammeduhh <online-ma@outlook.de>
2024-01-22 22:56:25 +0100

    l

--- src/components/CompanyComponents/TutorialCompany/TutorialCompany.jsx
+++ src/components/CompanyComponents/TutorialCompany/TutorialCompany.jsx
@@ -1,77 +1,108 @@
-import React from 'react';
-import { Container, Card, Row, Col, Carousel, Button } from 'react-bootstrap';
-import './TutorialCompany.css'
+import React, { useRef } from 'react';
+import { Container, Carousel, Button, } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import './TutorialCompany.css';
 
-const Tutorial = () => {
+const TutorialCompany = () => {
+  const carouselRef = useRef(null);
+
+  const handleNext = () => {
+    if (carouselRef.current) {
+      carouselRef.current.next();
+    }
+  };
+
+  const handlePrev = () => {
+    if (carouselRef.current) {
+      carouselRef.current.prev();
+    }
+  };
+
   return (
     <div className="tutorial-container">
-      <Container >
-        <Carousel>
+      <div className="tutorial-centered-container">
+        <Carousel ref={carouselRef} controls={false} interval={null}>
           <Carousel.Item>
             <img
               className="d-block w-100"
-              src=""
-              alt="First slide"
+              src="src/assets/pexels-vlada-karpovich-4050319.jpg"
+              alt="First Slide"
             />
             <Carousel.Caption>
               <h3>Willkommen bei WorkingStudent!</h3>
               <p>Klicken Sie sich durch das Tutorial, um die Seite besser kennenzulernen und optimal zu nutzen!</p>
             </Carousel.Caption>
           </Carousel.Item>
+
           <Carousel.Item>
             <img
               className="d-block w-100"
-              src=""
-              alt="Second slide"
+              src="https://via.placeholder.com/1200x600"
+              alt="Second Slide"
             />
             <Carousel.Caption>
               <h3>Home</h3>
-              <p>Entdecken Sie wöchentlich neue spannende Artikel und über die Navigationsbar kommen Sie zu den wichtigsten Funktionalitäten bei WorkingStudent!</p>
+              <p>Entdecken Sie wöchentlich neue spannende Artikel auf der Homepage. Über die Navigationsbar kommen Sie zu den wichtigsten Funktionalitäten!</p>
             </Carousel.Caption>
           </Carousel.Item>
+
           <Carousel.Item>
             <img
               className="d-block w-100"
-              src=""
-              alt="Second slide"
+              src="src/assets/chat.png"
+              alt="Third Slide"
             />
             <Carousel.Caption>
               <h3>Chat</h3>
               <p>Kommunizieren Sie mit potentiellen Werkstudenten über den Chat, um Sie näher kennenzulernen!</p>
             </Carousel.Caption>
           </Carousel.Item>
+
           <Carousel.Item>
             <img
               className="d-block w-100"
-              src=""
-              alt="Second slide"
+              src="https://via.placeholder.com/1200x600"
+              alt="Fourth Slide"
             />
             <Carousel.Caption>
               <h3>Matching</h3>
-              <p>Stöbern Sie durch interessante Studentenprofile, um genau die richtigen Werkstudenten für Ihr Unternehmen zu finden!</p>
+              <p>Stöbern Sie durch interessante Studentenprofile, um den richtigen Werkstudenten für Ihr Unternehmen zu finden!</p>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
             <img
               className="d-block w-100"
-              src=""
-              alt="Second slide"
+              src="src/assets/profil.png"
+              alt="Fifth Slide"
             />
             <Carousel.Caption>
               <h3>Profil</h3>
-              <p>Gestalten Sie ihr Profil nach Ihrem Ermessen, um Ihr Unternehmen so attraktiv wie möglich für Werkstudenten zu machen!</p>
+              <p>Beschreiben Sie Ihr Unternehmen über die Profilfunktion!</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="src/assets/kontakt.png"
+              alt="Sixth Slide"
+            />
+            <Carousel.Caption>
+              <h3>Kontakt</h3>
+              <p>Gerne nehmen wir Ihre Fragen, Anregungen oder Anfragen entgegen und stehen zur Verfügung!</p>
             </Carousel.Caption>
           </Carousel.Item>
         </Carousel>
-      </Container>
-      <Link to= '/HomeCompany'>
-      <Button variant="primary" className="tutorial-end-button">
-        Tutorial beenden
-      </Button>
+      </div>
+
+      <div className="tutorial-buttons-container">
+        <Button className='tutorial-buttons' id="tutorial-back-button" variant="primary" onClick={handlePrev}>Zurück</Button>{' '}
+        <Button className='tutorial-buttons' id="tutorial-next-button" variant="primary" onClick={handleNext}>Weiter</Button>{' '}
+      </div>
+      <Link to="/HomeCompany">
+        <Button className="tutorial-buttons" id="tutorial-end-button" variant="primary">Tutorial beenden</Button>
       </Link>
     </div>
   );
 };
 
-export default Tutorial;
+export default TutorialCompany;
